a892d9e6a7f3d819bbb36f3fef707271d1841e02
Afonso Ferreira <afonsohcferreira@gmail.com>
2022-04-01 01:17:42 -0300

    first commit

---
+++ PrimeiroPrograma.java
@@ -0,0 +1,9 @@
+public class PrimeiroPrograma {
+	public static void main(String args[]){
+		
+		int mundial = 3;
+		
+		System.out.println("\n Sao Paulo e sentimento que jamais acabara! \n");
+		System.out.println("\n Temos "+mundial+" Mundiais !");
+	}
+}
